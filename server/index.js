09744fe9de76c6a50bc3ef8649fbfcd5e14cf4fa
Rainey F <raineyfleming@protonmail.com>
2021-07-22 22:29:58 -0400

    fix server index

--- server/index.js
+++ server/index.js
@@ -1,15 +1,18 @@
+const path = require("path");
 const express = require("express");
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-app.get("/api", (req, res) => {
-    res.json({ message: "Make requests to the server at /api" });
-});
-
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
+});
+
+app.use(express.static(path.resolve(__dirname, "../client/build")));
+
+app.get("/api", (req, res) => {
+    res.json({ message: "Make requests to the server at /api" });
 });
 
 app.get("*", (req, res) => {
