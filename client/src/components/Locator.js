61311edab1ec8695800a42a97b6efb49e841c588
Rainey F <raineyfleming@protonmail.com>
2021-10-07 18:58:27 -0400

    set latitude and longitude for searched location and get weather

--- client/src/components/Locator.js
+++ client/src/components/Locator.js
@@ -4,6 +4,7 @@ function Locator() {
     const geolocationAvailable = "geolocation" in navigator;
     const [location, setLocation] = useState("");
     const [locations, setLocations] = useState([]);
+    const [latLon, setLatLon] = useState("");
     const handleLocationChange = async (e) => {
         setLocation(e.target.value);
         if (e.target.value.length >= 1) {
@@ -15,6 +16,9 @@ function Locator() {
             !locations.includes(e.target.value) &&
                 data.features &&
                 setLocations(data.features.map((result) => result.place_name));
+            const lat = data.features[0].geometry.coordinates[1];
+            const lon = data.features[0].geometry.coordinates[0];
+            setLatLon(lat + "," + lon);
         }
     };
     const useCurrentLocation = (e) => {
@@ -22,6 +26,7 @@ function Locator() {
         navigator.geolocation.getCurrentPosition(async (position) => {
             const latlon =
                 position.coords.latitude + "," + position.coords.longitude;
+            setLatLon(latlon);
             const url =
                 "/weather/" +
                 new URLSearchParams({ latlon: latlon }).get("latlon");
@@ -30,8 +35,13 @@ function Locator() {
             console.log(data);
         });
     };
-    const formSubmit = (e) => {
+    const formSubmit = async (e) => {
         e.preventDefault();
+        const url =
+            "/weather/" + new URLSearchParams({ latlon: latLon }).get("latlon");
+        const res = await fetch(url);
+        const data = await res.json();
+        console.log(data);
     };
     return (
         <form onSubmit={formSubmit} aria-labelledby="form-legend">
