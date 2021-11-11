0480619857d762b644513b30bb703b8e774e630b
Rainey F <raineyfleming@protonmail.com>
2021-11-11 08:55:32 -0500

    change geolocation pattern and how elements are hidden

--- client/src/components/Locator.js
+++ client/src/components/Locator.js
@@ -1,13 +1,43 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import WeatherPanels from "./WeatherPanels";
 
 function Locator() {
-    const geolocationAvailable = "geolocation" in navigator;
+    const [locationAvailable, setLocationAvailable] = useState(false);
+    const [latLon, setLatLon] = useState("");
+    const [currentLatLon, setCurrentLatLon] = useState("");
     const [location, setLocation] = useState("");
     const [locations, setLocations] = useState([]);
-    const [latLon, setLatLon] = useState("");
     const [weather, setWeather] = useState({});
     const [error, setError] = useState("");
+
+    const geolocationAvailable = () => {
+        if ("geolocation" in navigator) {
+            try {
+                navigator.geolocation.getCurrentPosition(
+                    (position) => {
+                        if (position.coords.latitude) {
+                            setLocationAvailable(true);
+                            const latlon =
+                                position.coords.latitude +
+                                "," +
+                                position.coords.longitude;
+                            setCurrentLatLon(latlon);
+                        } else {
+                            setLocationAvailable(false);
+                        }
+                    },
+                    (err) => {
+                        return setLocationAvailable(false);
+                    }
+                );
+            } catch (err) {
+                return setLocationAvailable(false);
+            }
+        } else {
+            return setLocationAvailable(false);
+        }
+    };
+
     const handleLocationChange = async (e) => {
         setLocation(e.target.value);
         if (e.target.value.length >= 1) {
@@ -24,23 +54,20 @@ function Locator() {
             setLatLon(lat + "," + lon);
         }
     };
-    const useCurrentLocation = (e) => {
+
+    const useCurrentLocation = async (e) => {
         e.preventDefault();
-        navigator.geolocation.getCurrentPosition(async (position) => {
-            const latlon =
-                position.coords.latitude + "," + position.coords.longitude;
-            setLatLon(latlon);
-            const url =
-                "/weather/" +
-                new URLSearchParams({ latlon: latlon }).get("latlon");
-            const res = await fetch(url);
-            const data = await res.json();
-            setWeather(data);
-        });
+        const url =
+            "/weather/" +
+            new URLSearchParams({ latlon: currentLatLon }).get("latlon");
+        const res = await fetch(url);
+        const data = await res.json();
+        setWeather(data);
     };
+
     const handleFormSubmit = async (e) => {
         e.preventDefault();
-        if (location === "") {
+        if (!locations.includes(location)) {
             setError("Please select a valid location");
             return;
         }
@@ -51,10 +78,19 @@ function Locator() {
         const data = await res.json();
         setWeather(data);
     };
+
     const handleNewLocation = (e) => {
         e.preventDefault();
         setWeather({});
+        setLocation("");
+        setLocations([]);
+        setError("");
     };
+
+    useEffect(() => {
+        geolocationAvailable();
+    }, []);
+
     return (
         <>
             {Object.keys(weather).length === 0 && (
@@ -63,16 +99,18 @@ function Locator() {
                         <legend id="form-legend">
                             Please provide a location
                         </legend>
-                        {geolocationAvailable && (
-                            <div className="form-row">
-                                <button onClick={useCurrentLocation}>
-                                    Use my Current Location
-                                </button>
-                            </div>
+                        {locationAvailable && (
+                            <>
+                                <div className="form-row">
+                                    <button onClick={useCurrentLocation}>
+                                        Use my Current Location
+                                    </button>
+                                </div>
+                                <div className="form-row">
+                                    <p>- or -</p>
+                                </div>
+                            </>
                         )}
-                        <div className="form-row">
-                            <p>- or -</p>
-                        </div>
                         <div className="form-row">
                             <label htmlFor="location">
                                 Search for a Location:
